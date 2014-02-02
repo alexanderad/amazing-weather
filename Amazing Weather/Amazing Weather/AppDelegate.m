2f0b119f87c3227a74f81cc272667bd99e6e72ec
Alexander Shchapov <alexanderad@gmail.com>
2014-02-02 12:13:22 +0200

    TODO file added.

--- Amazing Weather/Amazing Weather/AppDelegate.m
+++ Amazing Weather/Amazing Weather/AppDelegate.m
@@ -9,6 +9,13 @@
 #import "AppDelegate.h"
 #include <stdlib.h>
 
+
+#define API_KEY @"d8b1d8fe5065f29a130a8da1fedc6d7f"
+#define API_URL @"http://api.openweathermap.org/data/2.5/weather"
+#define CITY_ID 702550
+
+
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -20,7 +27,7 @@
     [statusItem setHighlightMode:YES];
     
     // start update timer to tick
-    updateTimer = [NSTimer scheduledTimerWithTimeInterval: (60 * 10) + 15
+    updateTimer = [NSTimer scheduledTimerWithTimeInterval: (60 * 15) + arc4random_uniform(25) // once in ~15 minutes
                                                    target: self
                                                  selector: @selector(onTick:)
                                                  userInfo: nil
@@ -30,39 +37,28 @@
 
 - (void)onTick:(NSTimer *) timer {
     
-    NSString *urlAsString = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?q=Lviv,ua&lang=ua&APPID=d8b1d8fe5065f29a130a8da1fedc6d7f"];
+    
+    
+    NSString *urlAsString = [NSString stringWithFormat:@"%@?id=%d&lang=ua&APPID=%@", API_URL, CITY_ID, API_KEY];
     NSURL *url = [[NSURL alloc] initWithString:urlAsString];
-    //NSLog(@"%@", urlAsString);
     
     [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:url] queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
         
         if (error) {
-            //[self.delegate fetchingGroupsFailedWithError:error];
-            NSLog(@"ERROR: %@", error);
-            [statusItem setTitle:@"n/a°C"];
+            NSLog(@"failed to fetch data: %@", error);
         } else {
-            NSLog(@"data received");
-            
-            
+            NSLog(@"data sucessfully received");
+    
             NSError *localError = nil;
             NSDictionary *parsedObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:&localError];
-            
-//            if (localError != nil) {
-//                *error = localError;
-//                return nil;
-//            }
-//            
-//            NSMutableArray *groups = [[NSMutableArray alloc] init];
-//            
             NSArray *results = [parsedObject valueForKey:@"main"];
+            
+            // fetch current temperature and convert it from Kelvin to Celsius
             double temp = [[results valueForKey:@"temp"] doubleValue];
             temp = temp - 273.15;
             [statusItem setTitle:[NSString stringWithFormat:@"%.1f°C", temp]];
-            
         }
     }];
-    
-    //http://api.openweathermap.org/data/2.5/weather?q=Lviv,ua&lang=ua&APPID=d8b1d8fe5065f29a130a8da1fedc6d7f
 }
 
 - (IBAction)quitAction:(id)sender {
---
+++ TODO.md
@@ -0,0 +1,14 @@
+TODO
+----
+
+### Add following to menu
+1. last updated at timestamp (click on last updated triggers instant update)
+2. related weather info: humidity, wind, temp range, etc
+3. info about current location selected
+
+### Also
+1. automatic location detection + notification about location change
+2. about window with reference to openweathermap API
+3. icon
+4. automatic timer fire after wake up
+5. connection errors handling (fair timer reschedule algo?)
